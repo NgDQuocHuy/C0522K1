cf68280c77113d240d569fa1b2b6e2b9b6bec3b4
QuocHuy <ngdquochuy241@gmail.com>
2022-06-12 07:48:24 +0700

    main js

--- Module 1/JS/thuchanhjs.js
+++ Module 1/JS/thuchanhjs.js
@@ -1,6 +1,14 @@
+//  bài tập 1
+
 function showMessage(){
     document.write('Xin chào!');
 }
 function showMessage(){
     console.log('Xin chào!');
+}
+
+//  bài tập 2
+
+function showMessage(){
+    alert('Xin chào!');
 }
