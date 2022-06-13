b4df689c5399e2950f8a23931154f9d3480bc107
QuocHuy <ngdquochuy241@gmail.com>
2022-06-13 16:09:00 +0700

    Điểm trung bình

---
+++ Module 1/JS/baitap/scores.js
@@ -0,0 +1,7 @@
+var physics = 9;
+var chemistry = 8;
+var biologycal = 7;
+var average = (physics + chemistry + biologycal) / 3;
+var total = physics + chemistry + biologycal;
+console.log(average);
+console.log(total);
