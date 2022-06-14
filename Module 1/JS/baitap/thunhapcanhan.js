137354c43bdc013ea4563a9601869a2b5a4cf607
QuocHuy <ngdquochuy241@gmail.com>
2022-06-14 10:39:34 +0700

    Thu nhập cá nhân

---
+++ Module 1/JS/baitap/thunhapcanhan.js
@@ -0,0 +1,22 @@
+var tncn = 5000000;
+if (tncn > 0 && tncn <= 5000000) {
+    console.log(phaidong = (tncn / 100) * 5 + 'VND');
+}
+else if (tncn > 5000000 && tncn <= 10000000) {
+    console.log(phaidong = (tncn / 100) * 10 + 'VND');
+}
+else if (tncn > 10000000 && tncn <= 18000000) {
+    console.log(phaidong = (tncn / 100) * 15 + 'VND');
+}
+else if (tncn > 18000000 && tncn <= 32000000) {
+    console.log(phaidong = (tncn / 100) * 20 + 'VND');
+}
+else if (tncn > 32000000 && tncn <= 52000000) {
+    console.log(phaidong = (tncn / 100) * 25 + 'VND');
+}
+else if (tncn > 52000000 && tncn <= 80000000) {
+    console.log(phaidong = (tncn / 100) * 30 + 'VND');
+}
+else if (tncn > 80000000) {
+    console.log(phaidong = (tncn / 100) * 35 + 'VND');
+}
