6e0ba6d3ff07c87f8c7da4eaea3befad3ee85311
QuocHuy <ngdquochuy241@gmail.com>
2022-06-16 11:35:38 +0700

    Tính tiền điện

--- Module 1/JS/baitap/electricity/EVN.js
+++ Module 1/JS/baitap/electricity/EVN.js
@@ -1,69 +1,95 @@
 
 function check() {
     let used = Number(document.getElementById("used").value);
+    let used2 = document.getElementById("used");
+    let bac1 = document.getElementById("bac1");
+    let bac2 = document.getElementById("bac2");
+    let bac3 = document.getElementById("bac3");
+    let bac4 = document.getElementById("bac4");
+    let bac5 = document.getElementById("bac5");
+    let bac6 = document.getElementById("bac6");
+    let total = document.getElementById("total");
+    let VAT = document.getElementById("VAT");
+    let all = document.getElementById("all");
+    const kwh1 = Number(used) * 1678;
+    const kwh2 = (Number(used) - 50) * 1734;
+    const kwh3 = (Number(used) - 100) * 2014;
+    const kwh4 = (Number(used) - 200) * 2536;
+    const kwh5 = (Number(used) - 300) * 2834;
+    const kwh6 = (Number(used) - 400) * 2927;
+    const monneyBac1 = 83900;
+    const monneyBac2 = 86700;
+    const monneyBac3 = 201400;
+    const monneyBac4 = 253600;
+    const monneyBac5 = 283400;
+    const thueVat = 100 * 10;
+    const total12 = 170600;
+    const total123 = 372000;
+    const total1234 = 6256000;
+    const total12345 = 909000;
     if (used > 0 && used <= 50) {
-        document.getElementById("bac1").value = Number(used) * 1678 + " VND";
-        document.getElementById("total").value = Number(used) * 1678 + " VND";
-        document.getElementById("VAT").value = (Number(used) * 1678) / 100 * 10 + " VND";
-        document.getElementById("all").value = Number(used) * 1678 + ((Number(used) * 1678) / 100 * 10) + " VND";
+        bac1.value = kwh1 + " VND";
+        total.value = kwh1 + " VND";
+        VAT.value = (kwh1) / thueVat + " VND";
+        all.value = kwh1 + ((kwh1) / thueVat) + " VND";
     }
     else if (used > 50 && used <= 100) {
-        document.getElementById("bac2").value = (Number(used) - 50) * 1734 + " VND";
-        document.getElementById("bac1").value = 83900 + " VND";
-        document.getElementById("total").value = (Number(used) - 50) * 1734 + 83900 + " VND";
-        document.getElementById("VAT").value = ((Number(used) - 50) * 1734 + 83900) / 100 * 10 + " VND";
-        document.getElementById("all").value = (Number(used) - 50) * 1734 + 83900 + (((Number(used) - 50) * 1734 + 83900) / 100 * 10) + " VND";
+        bac1.value = monneyBac1 + " VND";
+        bac2.value = kwh2 + " VND";
+        total.value = kwh2 + monneyBac1 + " VND";
+        VAT.value = (kwh2 + monneyBac1) / thueVat + " VND";
+        all.value = kwh2 + monneyBac1 + ((kwh2 + monneyBac1) / thueVat) + " VND";
     }
     else if (used > 100 && used <= 200) {
-        document.getElementById("bac3").value = (Number(used) - 100) * 2014 + " VND";
-        document.getElementById("bac1").value = 83900 + " VND";
-        document.getElementById("bac2").value = 86700 + " VND";
-        document.getElementById("total").value = (Number(used) - 100) * 2014 + 170600 + " VND";
-        document.getElementById("VAT").value = ((Number(used) - 100) * 2014 + 170600) / 100 * 10 + " VND";
-        document.getElementById("all").value = (Number(used) - 100) * 2014 + 170600 + (((Number(used) - 100) * 2014 + 170600) / 100 * 10) + " VND";
+        bac1.value = monneyBac1 + " VND";
+        bac2.value = monneyBac2 + " VND";
+        bac3.value = kwh3 + " VND";
+        total.value = kwh3 + total12 + " VND";
+        VAT.value = (kwh3 + total12) / thueVat + " VND";
+        all.value = kwh3 + total12 + ((kwh3 + total12) / thueVat) + " VND";
     }
     else if (used > 200 && used <= 300) {
-        document.getElementById("bac4").value = (Number(used) - 200) * 2536 + " VND";
-        document.getElementById("bac1").value = 83900 + " VND";
-        document.getElementById("bac2").value = 86700 + " VND";
-        document.getElementById("bac3").value = 201400 + " VND";
-        document.getElementById("total").value = (Number(used) - 200) * 2536 + 372000 + " VND";
-        document.getElementById("VAT").value = ((Number(used) - 200) * 2536 + 372000) / 100 * 10 + " VND";
-        document.getElementById("all").value = (Number(used) - 200) * 2536 + 372000 + (((Number(used) - 200) * 2536 + 372000) / 100 * 10) + " VND";
+        bac1.value = monneyBac1 + " VND";
+        bac2.value = monneyBac2 + " VND";
+        bac3.value = monneyBac3 + " VND";
+        bac4.value = kwh4 + " VND";
+        total.value = kwh4 + total123 + " VND";
+        VAT.value = (kwh4 + total123) / thueVat + " VND";
+        all.value = kwh4 + total123 + ((kwh4 + total123) / thueVat) + " VND";
     }
     else if (used > 300 && used <= 400) {
-        document.getElementById("bac5").value = (Number(used) - 300) * 2834 + " VND";
-        document.getElementById("bac1").value = 83900 + " VND";
-        document.getElementById("bac2").value = 86700 + " VND";
-        document.getElementById("bac3").value = 201400 + " VND";
-        document.getElementById("bac4").value = 253600 + " VND";
-        document.getElementById("total").value = (Number(used) - 300) * 2834 + 6256000 + " VND";
-        document.getElementById("VAT").value = ((Number(used) - 300) * 2834 + 6256000) / 100 * 10 + " VND";
-        document.getElementById("all").value = (Number(used) - 300) * 2834 + 6256000 + (((Number(used) - 300) * 2834 + 6256000) / 100 * 10) + " VND";
+        bac1.value = monneyBac1 + " VND";
+        bac2.value = monneyBac2 + " VND";
+        bac3.value = monneyBac3 + " VND";
+        bac4.value = monneyBac4 + " VND";
+        bac5.value = kwh5 + " VND";
+        total.value = kwh5 + total1234 + " VND";
+        VAT.value = (kwh5 + total1234) / thueVat + " VND";
+        all.value = kwh5 + total1234 + ((kwh5 + total1234) / thueVat) + " VND";
     }
     else if (used > 400) {
-        document.getElementById("bac6").value = (Number(used) - 400) * 2927 + " VND";
-        document.getElementById("bac1").value = 83900 + " VND";
-        document.getElementById("bac2").value = 86700 + " VND";
-        document.getElementById("bac3").value = 201400 + " VND";
-        document.getElementById("bac4").value = 253600 + " VND";
-        document.getElementById("bac5").value = 283400 + " VND";
-        document.getElementById("total").value = (Number(used) - 400) * 2927 + 909000 + " VND";
-        document.getElementById("VAT").value = ((Number(used) - 400) * 2927 + 909000) / 100 * 10 + " VND";
-        document.getElementById("all").value = (Number(used) - 400) * 2927 + 909000 + (((Number(used) - 400) * 2927 + 909000) / 100 * 10) + " VND";
+        bac1.value = monneyBac1 + " VND";
+        bac2.value = monneyBac2 + " VND";
+        bac3.value = monneyBac3 + " VND";
+        bac4.value = monneyBac4 + " VND";
+        bac5.value = monneyBac5 + " VND";
+        bac6.value = kwh6 + " VND";
+        total.value = kwh6 + total12345 + " VND";
+        VAT.value = (kwh6 + total12345) / thueVat + " VND";
+        all.value = kwh6 + total12345 + ((kwh6 + total12345) / thueVat) + " VND";
     }
 
 }
 function reset() {
-    document.getElementById("bac1").value = null;
-    document.getElementById("bac2").value = null;
-    document.getElementById("bac3").value = null;
-    document.getElementById("bac4").value = null;
-    document.getElementById("bac5").value = null;
-    document.getElementById("bac6").value = null;
-    document.getElementById("total").value = null;
-    document.getElementById("VAT").value = null;
-    document.getElementById("all").value = null;
-    document.getElementById("used").value = null;
-    document.getElementById("used").focus();
+    bac1.value = null;
+    bac2.value = null;
+    bac3.value = null;
+    bac4.value = null;
+    bac5.value = null;
+    bac6.value = null;
+    total.value = null;
+    VAT.value = null;
+    all.value = null;
+    used2.value = null;
+    used2.focus();
 }
