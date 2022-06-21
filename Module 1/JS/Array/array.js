55bb8c026b1db38430b87a350eebb7e384663d48
QuocHuy <ngdquochuy241@gmail.com>
2022-06-21 14:28:13 +0700

    Arrays

---
+++ Module 1/JS/Array/array.js
@@ -0,0 +1,14 @@
+var number = [5, 7, 8, 1, 4, 9, 3];
+// for (let i = 0; i < number.length; i++) {
+//     console.log(number[i]);
+// }
+let i = 0;
+// while (i < number.length) {
+//     console.log(number[i]);
+//     i++;
+// }
+do {
+    console.log(number[i]);
+    i++;
+}
+while (i < number.length);
---
+++ Module 1/JS/Array/bt.js
@@ -0,0 +1,4 @@
+let number = document.getElementById("num");
+number = [];
+number.length = document.getElementById("size")
+document.getElementById("view")
